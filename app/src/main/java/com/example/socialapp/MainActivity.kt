bfbc1cafcfc03a91a2da24302b8032815f7a1821
JaiminRana01 <jaiminr625@gmail.com>
2022-08-08 02:37:35 +0530

    added recycler view and adapter to main activity

--- app/src/main/java/com/example/socialapp/MainActivity.kt
+++ app/src/main/java/com/example/socialapp/MainActivity.kt
@@ -3,9 +3,20 @@ package com.example.socialapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.socialapp.daos.PostDao
+import com.example.socialapp.models.Post
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), IPostAdapter {
+
+    lateinit var adapter: PostAdapter
+    lateinit var postDao: PostDao
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,5 +25,31 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, CreatePostActivity::class.java)
             startActivity(intent)
         }
+
+        val db = FirebaseFirestore.getInstance()
+        postDao = PostDao()
+        val postCollection = db.collection("posts")
+        val query = postCollection.orderBy("createdAt", Query.Direction.DESCENDING)
+        val recyclerViewOptions =
+            FirestoreRecyclerOptions.Builder<Post>().setQuery(query, Post::class.java).build()
+        adapter = PostAdapter(recyclerViewOptions, this)
+
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        adapter.stopListening()
+    }
+
+    override fun onLikedClicked(postId: String) {
+        postDao.updateLikes(postId)
     }
 }
--- app/src/main/java/com/example/socialapp/daos/PostDao.kt
+++ app/src/main/java/com/example/socialapp/daos/PostDao.kt
@@ -2,8 +2,11 @@ package com.example.socialapp.daos
 
 import com.example.socialapp.models.Post
 import com.example.socialapp.models.User
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -27,6 +30,25 @@ class PostDao {
             val post = Post(text, user, currentTime)
 
             postCollection.document().set(post)
+        }
+    }
+
+    private fun getPostById(postId: String): Task<DocumentSnapshot> {
+        return postCollection.document(postId).get()
+    }
+
+    fun updateLikes(postId: String) {
+        GlobalScope.launch(Dispatchers.IO) {
+            val currentUserId = auth.currentUser!!.uid
+            val post = getPostById(postId).await().toObject(Post::class.java)!!
+            val isLiked = post.likedBy.contains(currentUserId)
+
+            if (isLiked) {
+                post.likedBy.remove(currentUserId)
+            } else {
+                post.likedBy.add(currentUserId)
+            }
+            postCollection.document(postId).set(post)
         }
     }
 }
